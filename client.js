d71ff88e5621d6bba0c6c4ce5c669de006fd2e42
Ian Escarro <ian.escarro@gmail.com>
2020-07-25 23:42:09 +0800

    Extended $

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
-class SkuGenerator {
-	static generate(name) {
-		$.get('api.php', { name: name }, function(data) {
-			console.log(data);
+var baseUrl = 'http://localhost:8888/intricateinlays/sku-generator'; //window.location.origin+window.location.pathname;
+console.log(baseUrl);
+
+$.fn.skuSuggest = function(code) {
+	$(this).each(function(i, e) {
+		$(e).change(function() {
+			var name = $(this).val();
+			$.get(baseUrl + '/api.php', { name: name }, function(data) {
+				console.log(data);
+				$(code).val(data.sku);
+			});
 		});
-	}
+	});
 }

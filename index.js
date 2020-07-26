7054c35960da86de250d7b738df3beb1d9834092
Ian Escarro <ian.escarro@gmail.com>
2020-07-26 23:55:41 +0800

    Moved base Url

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// var baseUrl = 'http://localhost:8888/intricateinlays/sku-generator';
-var baseUrl = 'http://sku-generator.intricateinlays.com';
-
 $.fn.suggestSku = function(code) {
+	// var baseUrl = 'http://localhost:8888/intricateinlays/sku-generator';
+	var baseUrl = 'http://sku-generator.intricateinlays.com';
 	$(this).each(function(i, e) {
 		$(e).change(function() {
 			var name = $(this).val();
-			$.get(baseUrl + '/api.php', { name: name }, function(data) {
+			var url = baseUrl + '/api.php';
+			console.log('lalala');
+			console.log(url);
+			$.get(url, { name: name }, function(data) {
 				console.log(data);
 				$(code).val(data.sku);
 			});
